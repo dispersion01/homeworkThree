c2052bfdb864898491d6bf38232c777960401d79
dispersion <anyta1901@gmail.com>
2022-01-10 12:57:59 +0300

    Комит во второй ветке с решением задач (исправленное)

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -8,35 +8,39 @@ public class Main {
     }
 
 
-    public static void exerciseOne(){
+    public static void exerciseOne() {
         int i = 1;
-            while ( i <=10 ) {
-                System.out.print(" " + i++);
-            }
-        System.out.println();
-            for (int a = 10; a >= 1; ){
-                System.out.print(" " + a--);
-            }
-        /* бесконечный цикл - не удается обойти
-        for (;  (i>=1) && (i <= 10);) {
-            System.out.print(" " + i--);
-        }*/
-   }
-   public static void exerciseTwo(){
-
-         for(int friday = 5; friday <= 31; friday = friday + 7) {
-             System.out.println("Сегодня пятница, " + friday +"-е число. Необходимо подготовить отчет.");
-         }
-   }
-
-   public static void exerciseThree(){
-        int cometCloseEarth = 79;
-        int startYear = 1817;
-        for (; (startYear <= 200) && (startYear >=100); startYear = startYear + cometCloseEarth) {
-            System.out.println(startYear);
+        while (i <= 10) {
+            System.out.print(" " + i++);
         }
-   }
+        System.out.println();
+        for (int a = 10; a >= 1; ) {
+            System.out.print(" " + a--);
+        }
+
+    }
+
+    public static void exerciseTwo() {
+        int firstFriday = 5;
+        int countOfDayInMonth = 31;
+        for (; firstFriday <= 31; firstFriday = firstFriday + 7) {
+            System.out.println("Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет.");
+        }
+    }
+
+    public static void exerciseThree() {
+        int year = 2010;
+        int yearBefore = year - 200;
+        int yearAfter = year + 100;
+        if (year > yearBefore)
+            for (; (year >= yearBefore) && (year <= yearAfter); year = year + 79) {
+                System.out.println(year);
+            }
+
+    }
 
 
 
 }
+
+
